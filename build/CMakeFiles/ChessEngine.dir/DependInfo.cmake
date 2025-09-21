94833cbc8cce794fc44cb08accc159176abe45cd
tadeyemo32 <tadeyemo32@gmail.com>
2025-09-21 07:34:01 +0100

    build system refactored

---
+++ build/CMakeFiles/ChessEngine.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kyuki/Chess-Engine/src/engine.cpp" "CMakeFiles/ChessEngine.dir/src/engine.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/src/engine.cpp.o.d"
+  "/home/kyuki/Chess-Engine/src/game.cpp" "CMakeFiles/ChessEngine.dir/src/game.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/src/game.cpp.o.d"
+  "/home/kyuki/Chess-Engine/src/grid.cpp" "CMakeFiles/ChessEngine.dir/src/grid.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/src/grid.cpp.o.d"
+  "/home/kyuki/Chess-Engine/src/main.cpp" "CMakeFiles/ChessEngine.dir/src/main.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ChessEngine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/kyuki/Chess-Engine/bin/ChessEngine"
+  "/home/kyuki/Chess-Engine/bin/ChessEngine.pdb"
+  "CMakeFiles/ChessEngine.dir/src/engine.cpp.o"
+  "CMakeFiles/ChessEngine.dir/src/engine.cpp.o.d"
+  "CMakeFiles/ChessEngine.dir/src/game.cpp.o"
+  "CMakeFiles/ChessEngine.dir/src/game.cpp.o.d"
+  "CMakeFiles/ChessEngine.dir/src/grid.cpp.o"
+  "CMakeFiles/ChessEngine.dir/src/grid.cpp.o.d"
+  "CMakeFiles/ChessEngine.dir/src/main.cpp.o"
+  "CMakeFiles/ChessEngine.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
